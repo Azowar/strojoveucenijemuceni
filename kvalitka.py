19e8e9ba6fa8aac2cef20e37a9ebedf0ab16b836
Azowar <95975878+Azowar@users.noreply.github.com>
2023-05-16 00:52:50 -0700

    Update kvalitka.py

--- kvalitka.py
+++ kvalitka.py
@@ -1,1 +1,54 @@
+import tensorflow as tf
+import os
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_dir = os.path.join("fruits-360", "Training")
+test_dir = os.path.join("fruits-360", "Test")
+
+batch_size = 32
+img_height = 100
+img_width = 100
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode="categorical"
+)
+
+test_data = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode="categorical"
+)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(131, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.CategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+epochs = 10
+history = model.fit(train_data, epochs=epochs, validation_data=test_data)
 
